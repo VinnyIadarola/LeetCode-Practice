795e961f02e25dc0ce4027bc9ceded7f9a9e82d8
Vincent Iadarola <144289702+VinnyIadarola@users.noreply.github.com>
2025-11-16 12:07:45 -0600

    [383] [Time Beats: 100.00%] [Memory Beats: 33.60%] - LeetPush

---
+++ Ransom-Note.c
@@ -0,0 +1,15 @@
+bool canConstruct(char* ransomNote, char* magazine) {
+    int hash[26] = {0};
+
+    for(int i = 0; magazine[i] != '\0'; i++) 
+        hash[magazine[i] - 'a']++;
+
+
+    for(int i = 0; ransomNote[i] != '\0'; i++) 
+        if(hash[ransomNote[i] - 'a']-- == 0)
+            return false;
+        
+    return true;
+
+}
+
