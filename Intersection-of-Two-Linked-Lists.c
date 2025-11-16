58a6c125abe3031dc939c891e86a48a3c1bb9036
Vincent Iadarola <144289702+VinnyIadarola@users.noreply.github.com>
2025-11-16 01:07:13 -0600

    [160] [Time Beats: 87.48%] [Memory Beats: 97.23%] - LeetPush

--- Intersection-of-Two-Linked-Lists.c
+++ Intersection-of-Two-Linked-Lists.c
@@ -6,33 +6,19 @@
  * };
  */
 struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
-    if(!headA || !headB) return NULL;
+   struct ListNode* p1 = headA;
+   struct ListNode* p2 = headB;
 
-  
-    struct ListNode* intersection = NULL;
+   while(p1 != p2) {
+        if(p1) 
+            p1 = p1->next;
+        else
+            p1 = headB;
 
-    
-    while(true) {
-        struct ListNode* currA = headA;
-        struct ListNode* currB = headB;
-
-        if(headA == intersection) break;
-        if(headB == intersection) break;
-
-
-        while((currA->next != intersection) || (currB->next != intersection)) {
-            currA = ((currA->next != intersection)) ? currA->next : currA;
-            currB = ((currB->next != intersection)) ? currB->next : currB;
-
-            if((currB == intersection) || (currA == intersection)) break;
-        }
-        
-        if(currA != currB) break;
-        
-        intersection = currA;
-
-    }
-
-    return intersection;
-
+        if(p2)
+            p2 = p2->next;
+        else 
+            p2 = headA;
+   }
+   return p1;
 }
