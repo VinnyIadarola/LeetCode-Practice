17dfd214f39a64a9442ae39ac63d9f597f7d80c0
Vincent Iadarola <144289702+VinnyIadarola@users.noreply.github.com>
2025-11-16 01:37:47 -0600

    [191] [Time Beats: 100.00%] [Memory Beats: 97.03%] - LeetPush

--- Number-of-1-Bits.c
+++ Number-of-1-Bits.c
@@ -1,8 +1,8 @@
 int hammingWeight(int n) {
     int count = 0;
-    while(n > 0){
-        count += (n & 1);
-        n >>= 1;
+    while(n){
+        count++;
+        n &= n - 1;
     }
     return count;
 }
