07bcd82f209fb7095988e18001c0ecabcae3ecc1
Vincent Iadarola <144289702+VinnyIadarola@users.noreply.github.com>
2025-11-18 16:40:38 -0600

    [111] [Time Beats: 89.63%] [Memory Beats: 88.06%] - LeetPush

--- Minimum-Depth-of-Binary-Tree.c
+++ Minimum-Depth-of-Binary-Tree.c
@@ -11,12 +11,12 @@
 int minDepthHelper(struct TreeNode* root) {
     if(!root) return 0;
 
-    int depth1 = minDepthHelper(root->left); 
-    int depth2 = minDepthHelper(root->right);
+    int depth1 = minDepthHelper(root->left) + 1; 
+    int depth2 = minDepthHelper(root->right) + 1;
 
-    if(depth1 == 0) return depth2 + 1;
-    if(depth2 == 0) return depth1 + 1;
-    return (depth1 > depth2) ? (depth2 + 1) : (depth1 + 1);
+    if(depth1 == 1) return depth2;
+    if(depth2 == 1) return depth1;
+    return (depth1 > depth2) ? (depth2) : (depth1);
     
 
 }
