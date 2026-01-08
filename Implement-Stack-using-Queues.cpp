c8c629f7e8d779bcacd54c21d9a341f43a997c19
Vincent Iadarola <144289702+VinnyIadarola@users.noreply.github.com>
2026-01-07 22:26:48 -0600

    [225] [Time Beats: 100.00%] [Memory Beats: 86.79%] - LeetPush

--- Implement-Stack-using-Queues.cpp
+++ Implement-Stack-using-Queues.cpp
@@ -6,46 +6,35 @@
 6    
 7    void push(int x) {
 8        q1.push(x);
-9    }
-10    
-11    int pop() {
-12        std::queue<int> q2;
-13
-14        while(q1.size() != 1) {
-15            q2.push(q1.front());
-16            q1.pop();
-17        }
-18
+9
+10        for(size_t i = 0; i < q1.size() - 1; ++i) {
+11            int val = q1.front();
+12            q1.pop();
+13            q1.push(val);
+14        }
+15
+16    }
+17    
+18    int pop() {
 19        int last = q1.front();
-20        q1 = q2;
+20        q1.pop();
 21        return last;
 22    }
 23    
 24    int top() {
-25        std::queue<int> q2;
-26
-27        while(q1.size() != 1) {
-28            q2.push(q1.front());
-29            q1.pop();
-30        }
-31
-32        int last = q1.front();
-33        q2.push(q1.front());
-34        q1 = q2;
-35        return last;
-36    }
-37    
-38    bool empty() {
-39        cout << q1.size() << endl;
-40        return q1.empty();
-41    }
-42};
-43
-44/**
-45 * Your MyStack object will be instantiated and called as such:
-46 * MyStack* obj = new MyStack();
-47 * obj->push(x);
-48 * int param_2 = obj->pop();
-49 * int param_3 = obj->top();
-50 * bool param_4 = obj->empty();
-51 */
+25        return q1.front();
+26    }
+27    
+28    bool empty() {
+29        return q1.empty();
+30    }
+31};
+32
+33/**
+34 * Your MyStack object will be instantiated and called as such:
+35 * MyStack* obj = new MyStack();
+36 * obj->push(x);
+37 * int param_2 = obj->pop();
+38 * int param_3 = obj->top();
+39 * bool param_4 = obj->empty();
+40 */
