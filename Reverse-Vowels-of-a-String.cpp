7aaefb9eef5cb71b2a93ac05136ae0fbe74e911c
Vincent Iadarola <144289702+VinnyIadarola@users.noreply.github.com>
2026-01-12 13:17:55 -0600

    [345] [Time Beats: 100.00%] [Memory Beats: 91.57%] - LeetPush

--- Reverse-Vowels-of-a-String.cpp
+++ Reverse-Vowels-of-a-String.cpp
@@ -7,7 +7,7 @@
 7
 8        
 9        while(forward < backward) {
-10            if(isVowel(forward) && isVowel(backward)) {
+10            if(isVowel(*forward) && isVowel(*backward)) {
 11                char temp = *forward;
 12                *forward = *backward;
 13                *backward = temp;
@@ -15,8 +15,8 @@
 15                backward--;
 16                forward++;
 17            } else {
-18                backward -= !isVowel(backward);
-19                forward += !isVowel(forward);
+18                backward -= !isVowel(*backward);
+19                forward += !isVowel(*forward);
 20            }
 21        }
 22
@@ -26,16 +26,8 @@
 26    }
 27
 28    private:
-29        inline bool isVowel(string::iterator it) {
-30            switch(tolower(static_cast<unsigned char>(*it))) {
-31                case 'a':
-32                case 'e':
-33                case 'i':
-34                case 'o':
-35                case 'u':
-36                    return true;
-37                default:
-38                    return false;
-39            }
-40        }
-41};
+29        inline bool isVowel(char c_) {
+30            unsigned char c = tolower(static_cast<unsigned char>(c_));
+31            return (c == 'a' || c == 'e' || c == 'o' || c == 'u' || c == 'i');
+32        }
+33};
