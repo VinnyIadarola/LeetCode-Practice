71fac6279ce52c7b16d6e18c48820272cee27e16
Vincent Iadarola <144289702+VinnyIadarola@users.noreply.github.com>
2026-08-01 12:04:26 -0400

    [511] [Time Beats: 86.92%] - LeetPush

---
+++ 511-Game-Play-Analysis-I.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT player_id, event_date AS first_login
+FROM Activity
+WHERE (player_id, event_date) IN (
+    SELECT player_id, MIN(event_date) AS event_date
+    FROM Activity
+    GROUP BY player_id
+)
