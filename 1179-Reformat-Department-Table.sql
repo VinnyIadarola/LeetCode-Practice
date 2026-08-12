cbc896a1a8569df334260d0b09bc7d8de4495e8a
Vincent Iadarola <144289702+VinnyIadarola@users.noreply.github.com>
2026-08-12 11:16:04 -0400

    [1179] [Time Beats: 84.91%] - LeetPush

--- 1179-Reformat-Department-Table.sql
+++ 1179-Reformat-Department-Table.sql
@@ -1,17 +1,16 @@
-# Write your MySQL query statement below
 SELECT 
     id,
-    MAX(IF(month = "Jan", revenue, NULL)) AS Jan_Revenue,
-    MAX(IF(month = "Feb", revenue, NULL)) AS Feb_Revenue,
-    MAX(IF(month = "Mar", revenue, NULL)) AS Mar_Revenue,
-    MAX(IF(month = "Apr", revenue, NULL)) AS Apr_Revenue,
-    MAX(IF(month = "May", revenue, NULL)) AS May_Revenue,
-    MAX(IF(month = "Jun", revenue, NULL)) AS Jun_Revenue,
-    MAX(IF(month = "Jul", revenue, NULL)) AS Jul_Revenue,
-    MAX(IF(month = "Aug", revenue, NULL)) AS Aug_Revenue,
-    MAX(IF(month = "Sep", revenue, NULL)) AS Sep_Revenue,
-    MAX(IF(month = "Oct", revenue, NULL)) AS Oct_Revenue,
-    MAX(IF(month = "Nov", revenue, NULL)) AS Nov_Revenue,
-    MAX(IF(month = "Dec", revenue, NULL)) AS Dec_Revenue
+    MAX(CASE WHEN month = "Jan" THEN revenue END) AS Jan_Revenue,
+    MAX(CASE WHEN month = "Feb" THEN revenue END) AS Feb_Revenue,
+    MAX(CASE WHEN month = "Mar" THEN revenue END) AS Mar_Revenue,
+    MAX(CASE WHEN month = "Apr" THEN revenue END) AS Apr_Revenue,
+    MAX(CASE WHEN month = "May" THEN revenue END) AS May_Revenue,
+    MAX(CASE WHEN month = "Jun" THEN revenue END) AS Jun_Revenue,
+    MAX(CASE WHEN month = "Jul" THEN revenue END) AS Jul_Revenue,
+    MAX(CASE WHEN month = "Aug" THEN revenue END) AS Aug_Revenue,
+    MAX(CASE WHEN month = "Sep" THEN revenue END) AS Sep_Revenue,
+    MAX(CASE WHEN month = "Oct" THEN revenue END) AS Oct_Revenue,
+    MAX(CASE WHEN month = "Nov" THEN revenue END) AS Nov_Revenue,
+    MAX(CASE WHEN month = "Dec" THEN revenue END) AS Dec_Revenue
 FROM Department
 GROUP BY id
