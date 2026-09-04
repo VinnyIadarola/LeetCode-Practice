c26c0ca492b9a099954af82c0f23935ed3e55b2f
Vincent Iadarola <144289702+VinnyIadarola@users.noreply.github.com>
2026-09-04 13:26:52 -0500

    [682] [Time Beats: 100.00%] [Memory Beats: 68.72%] - LeetPush

---
+++ 682-Baseball-Game.cpp
@@ -0,0 +1,45 @@
+#include <numeric>
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) {
+    os << "[ ";
+
+    for (const auto& x : vec) {
+        os << x << " ";
+    }
+
+    return os << "]";
+}
+
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        vector<string>::iterator ops = operations.begin();
+        vector<int> scores;
+        
+        scores.reserve(operations.size());
+
+        for(auto it = operations.begin(); it != operations.end(); ++it) {
+            string &op = *it;
+            const int end_idx = scores.size() - 1;
+            if(op == "+") {
+                const int sum_score = scores[end_idx] + scores[end_idx - 1];
+                scores.push_back(sum_score);
+            
+            } else if(op == "D") {
+                const int dub_score = scores[end_idx] * 2;
+                scores.push_back(dub_score);
+
+            } else if(op == "C") {
+                scores.erase(scores.end() - 1);
+
+
+            } else {
+                scores.push_back(stoi(op));
+            }
+        }
+
+        return accumulate(scores.begin(), scores.end(), 0);
+
+
+    }
+};
